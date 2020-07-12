e0029e756206b3db1b072b3736ca55ef1aeb2432
nickmly <nickmly@gmail.com>
2020-07-12 16:33:13 -0400

    removed landing

--- wp-content/themes/storefront-child/front-page.php
+++ wp-content/themes/storefront-child/front-page.php
@@ -8,21 +8,6 @@
 
 get_header(); ?>
 
-<div id="primary" class="front-page-landing-area">
-	<main id="main" class="site-main" role="main">
-		<a href="/shop" title="Go to the shop">
-			<div id="landing">
-				<div id="landing-image-container">
-					<div id="landing-image-overlay"></div>
-					<div id="landing-header">
-					</div>
-					<div id="landing-image"></div>
-				</div>
-			</div>
-		</a>
-
-	</main><!-- #main -->
-</div><!-- #primary -->
 <div class="front-page-content-area">
 	<?php
 	while (have_posts()) :
